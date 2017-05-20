23823dfec3f5d3f13e27fd8cbd62c41dbcfb91de
= <=>
2017-05-20 11:47:13 +0530

    Just

---
+++ 2048/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+typedef enum direction={
+	u,
+	d,
+	l,
+	r
+}
+
+int update(short int  * matrix, direction dir)
+{
+	int i,j,initPos;
+
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			if (matrix[])
+		}
+	}
+}
+int main()
+{
+	short int matrix [4][4] = {
+		{2, 2 ,4 ,4},
+		{2, -1, -1, 2},
+		{8, 4 ,4 , 16},
+		{-1,-1,-1,-1}
+	}
+
+	update (matrix,r);
+}
